8a8eec6162d3634486845cc37a28ed02b224cabd
James McPherson <mcpherson.jamesd@gmail.com>
2017-11-18 12:59:24 -0800

    Initialised user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+    # before_action :set_event, only: [:show, :edit, :update, :destroy]
+
+    def index
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def edit
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy    
+    end
+
+    private
+    #   def set_event
+    #     @event = Event.find(params[:id])
+    #   end
+
+    #   def event_params
+    #     params.require(:event).permit(:type, :name, :location, :description, :user_id, :leader_id, :date, :start_time, :end_time)
+    #   end
+end
