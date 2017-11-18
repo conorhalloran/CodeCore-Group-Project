017856173ab8b544422c47b71f1e6c356b5ba67c
Sam Bukhari <sambukhari04@gmail.com>
2017-11-18 15:49:18 -0800

    Setup Admin

--- db/schema.rb
+++ db/schema.rb
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20171119001719) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "is_admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
