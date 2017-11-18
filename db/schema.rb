0ca04eaace8df91d68d43bb0f0becce310625b85
conorhalloran <conorhalloran@gmail.com>
2017-11-18 11:56:48 -0800

    Change Start_Time & End_Time to String

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171118191111) do
+ActiveRecord::Schema.define(version: 20171118195334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,8 @@ ActiveRecord::Schema.define(version: 20171118191111) do
     t.text "description"
     t.bigint "user_id"
     t.date "date"
-    t.time "start_time"
-    t.time "end_time"
+    t.string "start_time"
+    t.string "end_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_events_on_user_id"
