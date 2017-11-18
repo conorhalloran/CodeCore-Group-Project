dc9170e6c9a2883be19a9eaf9f967493d381a5f2
James McPherson <mcpherson.jamesd@gmail.com>
2017-11-18 15:26:12 -0800

    Set up users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-    # before_action :set_event, only: [:show, :edit, :update, :destroy]
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+    before_action :currect_user
 
     def index
     end
@@ -8,26 +9,50 @@ class UsersController < ApplicationController
     end
 
     def new
+        @user = User.new
     end
 
     def edit
     end
 
     def create
+        @user = User.new(user_params)
+        
+        respond_to do |format|
+          if @event.save
+                # success
+            else
+                # redirect
+            end
+        end
     end
 
     def update
+        respond_to do |format|
+            if @user.update(user_params)
+                format.html { redirect_to @user, notice: 'User was successfully updated.' }
+                format.json { render :show, status: :ok, location: @event }
+            else
+                format.html { render :edit }
+                format.json { render json: @user.errors, status: :unprocessable_entity }
+          end
+        end
     end
 
-    def destroy    
+    def destroy
+        @user.destroy
+        respond_to do |format|
+            format.html { redirect_to user_url, notice: 'User was successfully destroyed.' }
+            format.json { head :no_content }
+        end
     end
 
     private
-    #   def set_event
-    #     @event = Event.find(params[:id])
-    #   end
+        def set_user
+            @event = Event.find(params[:id])
+         end
 
-    #   def event_params
-    #     params.require(:event).permit(:type, :name, :location, :description, :user_id, :leader_id, :date, :start_time, :end_time)
-    #   end
+        def user_params
+            params.require(:event).permit(:name, :email, :phone_number, :password, :password_confirmation)
+        end
 end
