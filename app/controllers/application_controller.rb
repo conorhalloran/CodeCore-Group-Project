1aedbec230a19419d7a5a366f38fd89383612449
Ian Edmonds <ianladneredmonds@gmail.com>
2017-11-17 22:32:56 -0800

    user model, sign-in, seeds

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def user_signed_in?
+    session[:user_id].present?
+  end
+  helper_method :user_signed_in?
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  helper_method :current_user
+
+  private
+
+  def authenticate_user!
+    if !user_signed_in?
+      redirect_to new_session_path, alert: 'You must sign in or sign up first!'
+    end
+  end
 end
