d0bd7f0937911a368c768feec8a8edf537d6166b
Steve Kim <dkim.steve@gmail.com>
2017-11-19 10:50:12 -0800

    resolved conflict

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,16 +4,13 @@ class Event < ApplicationRecord
 
   belongs_to :user
 
-<<<<<<< HEAD
 
 
   # def to_param
   #   "#{id}-#{title}".parameterize
   # end
-=======
   def self.search(search)
     where("location ILIKE ?", "%#{search}%") 
   end
 
->>>>>>> added search engine to search events by location
 end
