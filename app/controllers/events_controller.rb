74508132ff0d6e0250c9f26c954dd836cb64e729
Ian Edmonds <ianladneredmonds@gmail.com>
2017-11-18 17:09:02 -0800

    calendar data complete

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -74,7 +74,7 @@ class EventsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def event_params
-    params.require(:event).permit(:event_type, :name, :location, :description, :user_id, :leader_id, :date, :start_time, :end_time)
+    params.require(:event).permit(:type, :name, :location, :description, :user_id, :leader_id, :date, :start_time, :end_time)
   end
 
   def authorize_user!
