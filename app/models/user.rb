c96fdb85a43ccb24543f90c9a5a5cb232bfde085
James McPherson <mcpherson.jamesd@gmail.com>
2017-11-18 19:22:45 -0800

    Fix problems with user auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :events, dependent: :nullify
+
   before_save { self.email = email.downcase }
   validates :first_name, presence: true
   validates :last_name, presence: true
