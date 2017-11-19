9dede86e96577a5d2adef145ebd6e749fe24e90e
Calvin Hui <cowfun87@gmail.com>
2017-11-18 18:25:04 -0800

    Calendar styling

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,7 @@ module GroupProject
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.beginning_of_week = :sunday
+    config.time_zone = 'Pacific Time (US & Canada)'
   end
 end
