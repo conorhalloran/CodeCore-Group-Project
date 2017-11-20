f1069afb0a5e9e4fd7ba605cbf74350cfc8a6e3e
Conor Halloran <conorhalloran@gmail.com>
2017-11-19 21:07:20 -0800

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -92,8 +92,8 @@ ActiveRecord::Schema.define(version: 20171119230136) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-    t.boolean "is_admin", default: false
     t.string "phone_number"
+    t.boolean "is_admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
